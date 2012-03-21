70d8dacd4a96011967e7e439b96ad3bb782e3010
UstcDirty <wang.rusticwolf@gmail.com>
2012-03-21 13:44:52 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DirtyBlog::Application.config.secret_token = 'f56a1488fa331bfc04af0714363ccb54e5e6120428cc0b91854517bed6f5fa1910666c335f0fd13291ead0090350a73726a0b587f77bb987e2591522205b476a'
