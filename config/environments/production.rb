623cc935660be91cc7bd50dfcff0891b3d0def0a
UstcDirty <wang.rusticwolf@gmail.com>
2012-03-22 19:46:55 +0800

    restore to previous state in config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,9 +15,9 @@ DirtyBlog::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
-  #config.assets.compile = true 
-  config.assets.precompile += %w[application.css]
+  # config.assets.compile = false
+  config.assets.compile = true 
+  # config.assets.precompile += %w[application.css]
   # Generate digests for assets URLs
   config.assets.digest = true
 
